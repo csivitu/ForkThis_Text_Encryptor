bc251b45467a61fc370d67c4f333b04098f55077
MathavaKumaar2025 <mathavakumaar27@gmail.com>
2025-09-27 09:11:55 +0530

    file added

--- gui.py
+++ gui.py
@@ -1,14 +1,27 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import Button
 import logic
 import pyperclip
 import pygame as pg
 from tkinter import filedialog
+import time
+
 
 pg.mixer.init()
 pg.mixer.music.load("buttonClick.mp3")
+hover_sound = pg.mixer.Sound("sound.mp3")
+last_hover_time = 0
+
 def buttonSound():
     pg.mixer.music.play()
+
+def play_hover_sound(event=None):
+    global last_hover_time
+    current_time = time.time()
+    if current_time - last_hover_time > 0.3:  # 300ms cooldown
+        hover_sound.play()
+        last_hover_time = current_time
 
 def handle_generate_key():
     new_key = logic.generate_key_str()
@@ -16,10 +29,10 @@ def handle_generate_key():
     key_entry.insert(0,new_key)
     buttonSound()
 
-
 def handle_encrypt():
     message = message_entry.get()
     key = key_entry.get()
+    
 
     if not message or not key:
         messagebox.showwarning("Input Error", "Message and Key fields are required.")
@@ -108,6 +121,8 @@ key_entry.grid(row=3,column=0,columnspan=2, sticky="ew", pady=(0, 10))
 
 key_button = tk.Button(input_frame,text="Generate Key",command=handle_generate_key,bg=buttonsColor)
 key_button.grid(row=4,column=0,columnspan=2,pady=(0,10))
+key_button.bind("<Enter>", play_hover_sound)    ### >>> [ADDED]
+key_button.bind("<Motion>", play_hover_sound)
 
 #Buttons
 button_frame = tk.LabelFrame(frame,padx=10,pady=5,background=otherFrames)
@@ -115,15 +130,23 @@ button_frame.pack(fill='x',padx=10,pady=10)
 
 encrypt_button = tk.Button(button_frame, text="Encrypt",command=handle_encrypt,bg=buttonsColor)
 encrypt_button.pack(side="left", padx=50)
+encrypt_button.bind("<Enter>", play_hover_sound)
+encrypt_button.bind("<Motion>", play_hover_sound)
 
 decrypt_button = tk.Button(button_frame, text="Decrypt",command=handle_decrypt,bg=buttonsColor)
 decrypt_button.pack(side="left", padx=50)
+encrypt_button.bind("<Enter>", play_hover_sound)
+encrypt_button.bind("<Motion>", play_hover_sound)
 
 clear_button = tk.Button(button_frame, text="Clear",command=handle_clear,bg=buttonsColor)
 clear_button.pack(side="left", padx=50)
+clear_button.bind("<Enter>", play_hover_sound)
+clear_button.bind("<Motion>", play_hover_sound)
 
 encrypt_file_button = tk.Button(button_frame, text="Encrypt File", command=handle_encrypt_file, bg=buttonsColor)
 encrypt_file_button.pack(side="left", padx=10)
+encrypt_button.bind("<Enter>", play_hover_sound)
+encrypt_button.bind("<Motion>", play_hover_sound)
 
 
 
